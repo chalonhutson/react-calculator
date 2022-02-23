eb3a3ea75129f36ecf614ea202fb3e50a4fd81cf
chalonhutson <chalonhutson@gmail.com>
2022-02-22 18:36:12 -0700

    Adjusting CSS on calculator.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import logo from './logo.svg'
 import './App.css'
-import Button from './components/Button';
+import Button from './components/ButtonNumber';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className='ButtonRow'>
+        <div className='buttonRow'>
           <Button number={1} />
           <Button number={2} />
           <Button number={3} />
         </div>
-        <div className='ButtonRow'>
+        <div className='buttonRow'>
           <Button number={4} />
           <Button number={5} />
           <Button number={6} />
         </div>
-        <div className='ButtonRow'>
+        <div className='buttonRow'>
           <Button number={7} />
           <Button number={8} />
           <Button number={9} />
         </div>
-        <div className='ButtonRow'>
+        <div className='buttonRow'>
           <Button number={0} />
         </div>
       </header>
--- src/components/ButtonNumber.js
+++ src/components/ButtonNumber.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import '../App.css'
 
 export default function Button({ number }) {
   return (
     <div>
-        <button onClick={() => console.log(`Pressed ${number.toString()}`)}>
+        <button className='buttonNumber' onClick={() => console.log(`Pressed ${number.toString()}`)}>
             {number.toString()}
         </button>
     </div>
